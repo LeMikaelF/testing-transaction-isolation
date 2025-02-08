5c67175390d55faf738843837df84982923cc217
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2025-02-07 20:47:42 -0500

    add isolation test

--- src/test/java/com/mikaelfrancoeur/mysqltransactiontest/MysqlTransactionTestApplicationTests.java
+++ src/test/java/com/mikaelfrancoeur/mysqltransactiontest/MysqlTransactionTestApplicationTests.java
@@ -1,15 +1,100 @@
 package com.mikaelfrancoeur.mysqltransactiontest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.dao.CannotAcquireLockException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
 
-@Import(TestcontainersConfiguration.class)
+import lombok.SneakyThrows;
+
 @SpringBootTest
-class MysqlTransactionTestApplicationTests {
+@Import(TestcontainersConfiguration.class)
+class MysqlTransactionTestApplicationTests implements WithAssertions {
+
+    private static final ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+
+    private TransactionTemplate transactionTemplate;
+
+    @BeforeEach
+    void beforeEach() {
+        transactionTemplate = new TransactionTemplate(platformTransactionManager);
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_REPEATABLE_READ);
+
+        jdbcTemplate.execute("drop table if exists t");
+        jdbcTemplate.execute("create table t (num int)");
+    }
 
     @Test
-    void contextLoads() {
+    @SneakyThrows
+    void testIsolation() {
+        for (int numDeadlocks = 0; numDeadlocks < 1;) {
+            Exception deadlock = catchThrowableOfType(CannotAcquireLockException.class, this::raceSqlThreads);
+
+            if (deadlock != null) {
+                numDeadlocks++;
+                continue;
+            }
+
+            assertNoPhantomRead();
+        }
+    }
+
+    private void raceSqlThreads() throws InterruptedException, ExecutionException {
+        jdbcTemplate.execute("truncate t");
+
+        // language=SQL
+        String insertSql = "insert into t select (select 1 where not exists (select * from t where num = 1))";
+
+        CountDownLatch latch = new CountDownLatch(1);
+
+        //noinspection Convert2Lambda
+        Future<?> otherThread = executorService.submit(new Runnable() {
+            @Override
+            @SneakyThrows
+            public void run() {
+
+                transactionTemplate.execute(_ -> {
+                    try {
+                        latch.await();
+                        jdbcTemplate.execute(insertSql);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return null;
+                });
+            }
+        });
+
+        latch.countDown();
+        jdbcTemplate.execute(insertSql);
+
+        otherThread.get();
+    }
+
+    private void assertNoPhantomRead() {
+        Integer count = jdbcTemplate.queryForObject("select count(*) from t where num = 1", Integer.class);
+        assertThat(count)
+                .withFailMessage("found phantom read, transaction is not properly isolated")
+                .isEqualTo(1);
     }
 
 }
