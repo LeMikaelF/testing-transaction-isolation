b06b7c99233a80e5b58537ef977fca65059cdbf7
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2025-02-07 20:09:18 -0500

    initial commit

---
+++ src/main/java/com/mikaelfrancoeur/mysqltransactiontest/MysqlTransactionTestApplication.java
@@ -0,0 +1,13 @@
+package com.mikaelfrancoeur.mysqltransactiontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlTransactionTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlTransactionTestApplication.class, args);
+    }
+
+}
