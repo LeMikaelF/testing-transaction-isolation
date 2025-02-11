801178c7dbe4de04ee34d9fe4584946b26c8574d
Mikaël Francoeur <mikael.francoeur@ticketmaster.com>
2025-02-10 21:22:48 -0500

    optimize imports

--- src/test/java/com/mikaelfrancoeur/mysqltransactiontest/TestcontainersConfiguration.java
+++ src/test/java/com/mikaelfrancoeur/mysqltransactiontest/TestcontainersConfiguration.java
@@ -1,6 +1,5 @@
 package com.mikaelfrancoeur.mysqltransactiontest;
 
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
